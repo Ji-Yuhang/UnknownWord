b90bc06c9e5bd32aa5a32ecda2cb623d4cbc8054
Ji-Yuhang <yuhang.silence@gmail.com>
2015-07-12 17:13:00 +0800

    add will_word.rb

---
+++ help/will_word.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+require 'active_record'
+#require 'sqlite3'
+require 'awesome_print'
+#require '../ruby/spell'
+ActiveRecord::Base.establish_connection :adapter => "sqlite3",
+    :database => "../db/the_will_power_instinct.db"
+class Articles < ActiveRecord::Base
+end
+
+class Sentences < ActiveRecord::Base
+end
+
+class Words < ActiveRecord::Base
+end
+
+Words.all().each do |wordinfo|
+    ap wordinfo
+end
+
+=begin
+spell = Spell.instance
+io = File.open "unknown_word.txt"
+io.each_line do |line|
+    line.chomp!
+    if spell.check? line
+        word = spell.remove_plural line
+        num = word.to_i
+        if num != 0
+            next
+        end
+    
+        #ap word
+        wordinfos = Word.where word: word
+        if wordinfos.empty?
+            wordinfo = Word.new
+            wordinfo.word = word
+            #wordinfo.save
+            ap wordinfo
+        else 
+            wordinfo = wordinfos.first
+            id = wordinfo.word_id
+            ap id
+            un = Unknownword.new
+            un.word_id = id
+            un.familiarity = 1 # 1 for known, 0 for unknown
+            un.save
+            
+        end
+        
+    end
+end
+Unknownword.all().each do |un|
+    word_id = un.word_id
+    if un.familiarity == 1
+        #ap un.content
+
+        begin
+            word = Wordlist.find word_id
+            ap word.content
+            io.puts word.content
+        rescue ActiveRecord::RecordNotFound
+            un.destroy
+            un.save!
+            ap "not found"
+            next
+        end
+
+    end
+end
+=end
